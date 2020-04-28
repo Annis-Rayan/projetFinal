f8d4834a077b6ce0b88c8fec8a26ef25e45a9e98
Thomas347 <thomas.guinebert15@gmail.com>
2020-04-28 17:18:56 +0200

    TestThomas

--- src/test/java/formationJpa/TestThomas.java
+++ src/test/java/formationJpa/TestThomas.java
@@ -10,11 +10,25 @@ public class TestThomas {
 
 	public static void main(String[] args) {
 		
-		Persistence.createEntityManagerFactory("projetsopra");
+		Persistence.createEntityManagerFactory("projetfinal");
 		
 		DaoAnimal daoAnimal=DaoAnimalFactory.getInstance();
 		
 		Animal animal = new Animal();
+		
+		animal.setNomCourant("Mesange charbonniere");
+		daoAnimal.insert(animal);
+		
+
+		Animal animal1 = new Animal();
+		
+		animal1.setNomCourant("Pinson");
+		daoAnimal.insert(animal1);
+
+		Animal animal2 = new Animal();
+		
+		animal.setNomCourant("Rossignol philomele");
+		daoAnimal.insert(animal2);
 	}
 
 }
