7bd87fb91a2495d044957cdd1f0cac26b758b2a5
jonathantorelsalat <jonath_ts@hotmail.fr>
2020-04-28 16:26:31 +0200

    observation

--- src/main/java/projetFinal/entity/Observation.java
+++ src/main/java/projetFinal/entity/Observation.java
@@ -14,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
@@ -38,13 +39,13 @@ public class Observation {
 	private Integer nombre;
 	@Column(name="description",nullable=false)
 	private String description;
-	@OneToOne()
+	@ManyToOne
 	@JoinColumn(name = "localisation", foreignKey = @ForeignKey(name = "localisation_fk"))
 	private Localisation localisation;
-	@OneToOne()
+	@ManyToOne
 	@JoinColumn(name = "utilisateur", foreignKey = @ForeignKey(name = "utilisateur_fk"))
 	private Utilisateur utilisateur;
-	@OneToOne()
+	@ManyToOne
 	@JoinColumn(name = "animal", foreignKey = @ForeignKey(name = "animal_fk"))
 	private Animal animal;
 	@OneToMany(mappedBy="observation")
--- src/test/java/formationJpa/TestJonathan.java
+++ src/test/java/formationJpa/TestJonathan.java
@@ -1,5 +1,8 @@
 package formationJpa;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import projetFinal.dao.DaoObservation;
 import projetFinal.dao.DaoObservationFactory;
 import projetFinal.entity.Observation;
@@ -7,12 +10,22 @@ import projetFinal.entity.Observation;
 public class TestJonathan {
 	public static void main(String[] args) {
 		Observation obs = new Observation();
+		
 		System.out.println("-----------------------------------");
 		DaoObservation daoObservation=DaoObservationFactory.getInstance();
 		System.out.println("-----------------------------------");
 		
-		obs.setNombre(2);		
-		obs.setId(100);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			obs.setDateObservation(sdf.parse("10/02/2019"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		obs.setNombre(2);	
+		obs.setDescription("description");
+		obs.setId(1);
 		System.out.println("-----------------------------------");
 
 		daoObservation.insert(obs);
