9e46b719ceaca648d4739807878f047b14972b5e
Thomas347 <thomas.guinebert15@gmail.com>
2020-04-28 12:12:28 +0200

    enum ordre

---
+++ src/main/java/projetFinal/entity/Ordre.java
@@ -0,0 +1,17 @@
+package projetFinal.entity;
+
+public enum Ordre {
+	
+	Mammifere("Mammifere"), Insecte("Insecte"), Oiseaux("Oiseaux"), Amphibiens("Amphibiens"); 
+
+	private String label;
+
+	private Ordre(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+	
+}
