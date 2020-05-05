9776eab1c6f145b97739a867dcde03d2455fe8bc
Thomas347 <thomas.guinebert15@gmail.com>
2020-05-05 11:46:32 +0200

    update AnimalSerice

--- src/main/java/projetFinal/service/AnimalService.java
+++ src/main/java/projetFinal/service/AnimalService.java
@@ -1,5 +1,90 @@
 package projetFinal.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import projetFinal.entity.Animal;
+import projetFinal.entity.Localisation;
+import projetFinal.exception.NoAnimalFoundException;
+import projetFinal.repository.AnimalRepository;
+
+
+
+@Service
 public class AnimalService {
 
+	@Autowired
+	private AnimalRepository animalRepository;
+
+	public void ajout(Animal animal) {
+		
+		if (animal.getNomScientifique().isEmpty()) {
+			animal.setNomScientifique("non defini");
+		}
+		if (animal.getDescription().isEmpty()) {
+			animal.setDescription("non defini");
+		}
+		if (animal.getEmplacementImage().isEmpty()) {
+			animal.setEmplacementImage("non defini");
+		}
+		
+		animalRepository.save(animal);
+	}
+
+	public Animal miseAjour(Animal animal) {
+		Optional<Animal> opt = animalRepository.findById(animal.getId());
+		//Optional<Animal> opt2 = animalRepository.findByNomCourant(animal.getNomCourant());
+		if (opt.isPresent()) {
+			Animal animalEnBase = opt.get();
+			if (animal.getNomCourant() != null) {
+				animalEnBase.setNomCourant(animal.getNomCourant());
+			}
+			if (animal.getNomScientifique() != null) {
+				animalEnBase.setNomScientifique(animal.getNomScientifique());
+			}
+			if (animal.getDescription() != null) {
+				animalEnBase.setDescription(animal.getDescription());
+			}
+			if (animal.getEmplacementImage() != null) {
+				animalEnBase.setEmplacementImage(animal.getEmplacementImage());
+			}
+			if (animal.getOrdre() != null) {
+				animalEnBase.setOrdre(animal.getOrdre());
+			}
+			if (animal.getObservations() != null) {
+				animalEnBase.setObservations(animal.getObservations());
+			}
+			
+			animalRepository.save(animalEnBase);
+		} else {
+			return null;
+		}
+		return animal;
+		// throw new NoAnimalFoundException()
+	}
+	
+	public Animal recherche (Integer id) throws NoAnimalFoundException {
+		
+		Optional<Animal> opt = animalRepository.findById(id);
+		if (opt.isPresent()) {
+			return opt.get();
+		
+			} 
+		else {
+			throw new NoAnimalFoundException();
+		}		
+				
+	} 
+	
+	public boolean suppression(Integer id) {
+		Optional<Animal> opt = animalRepository.findById(id);
+		if (opt.isPresent()) {
+			animalRepository.deleteById(id);
+			return true;
+
+		}
+		return false;
+	}
 }
