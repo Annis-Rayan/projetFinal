866bd4b0b5cc2e8a6ad755a0eb8f6e49aec90f26
Raphael <raphael.muller@ens.uvsq.fr>
2020-05-05 10:08:36 +0200

    add localisationservice

--- src/main/java/projetFinal/service/LocalisationService.java
+++ src/main/java/projetFinal/service/LocalisationService.java
@@ -1,7 +1,55 @@
 package projetFinal.service;
 
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import projetFinal.entity.Localisation;
+import projetFinal.exception.NolocalisationFoundException;
+import projetFinal.repository.LocalisationRepository;
+
+@Service
 public class LocalisationService {
+	
+	@Autowired
+	private LocalisationRepository locrep;
+	
+	public void ajout(Localisation localisation) {
+		if (localisation.getLocalite().isEmpty()) {
+			localisation.setLocalite("non defini");
+		}
+		locrep.save(localisation);
+	}
+	
+	public Localisation miseAjour(Localisation localisation) throws NolocalisationFoundException {
+		Optional<Localisation> opt = locrep.findById(localisation.getID());
+		if (opt.isPresent()) {
+			Localisation locenbase = opt.get();
+			if (localisation.getPays() != null) {
+				locenbase.setPays(localisation.getPays());
+			}
+			if (localisation.getRegion() != null) {
+				locenbase.setRegion(localisation.getRegion());
+			}
+			
+			
+			locenbase.setLocalite(localisation.getLocalite());
+			locrep.save(locenbase);
+			return locenbase;
+		} else {
+			throw new NolocalisationFoundException();
+		}
+		 
+
+	}
+	
+	public Localisation recherche(Integer id) {
+		Optional<Localisation> opt=locrep.findById(id);
+		if(opt.isPresent()) {
+			return opt.get();
+		}
+		throw new IllegalArgumentException();
+	}
 	
 }
