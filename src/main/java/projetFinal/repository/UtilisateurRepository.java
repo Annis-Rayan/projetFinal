1ba0b2a8665e9a2a91cec60a29937c873fbab7b8
Annis-Rayan <annisr.b@gmail.com>
2020-05-05 09:31:57 +0200

    creation repository

---
+++ src/main/java/projetFinal/repository/UtilisateurRepository.java
@@ -0,0 +1,9 @@
+package projetFinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import projetFinal.entity.Utilisateur;
+
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
+
+}
