35cd3c70351491d771d5ed8261b5b9866e6488b8
Raphael <raphael.muller@ens.uvsq.fr>
2020-05-05 09:47:18 +0200

    add llocalisation rep

---
+++ src/main/java/projetFinal/repository/LocalisationRepository.java
@@ -0,0 +1,10 @@
+package projetFinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import projetFinal.entity.Localisation;
+
+
+public interface LocalisationRepository extends JpaRepository<Localisation, Integer> {
+
+}
