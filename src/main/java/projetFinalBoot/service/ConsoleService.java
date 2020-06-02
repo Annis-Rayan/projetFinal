d3130b0532f7b90209dc0e87c8f00861b4ee9bff
Annis-Rayan <annisr.b@gmail.com>
2020-06-02 17:37:35 +0200

    modif security2

--- src/main/java/projetFinalBoot/service/ConsoleService.java
+++ src/main/java/projetFinalBoot/service/ConsoleService.java
@@ -1,15 +1,29 @@
 package projetFinalBoot.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import projetFinalBoot.entity.Login;
+import projetFinalBoot.repository.LoginRepository;
 
 
 @Service
 public class ConsoleService implements CommandLineRunner {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	LoginRepository loginRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		// System.out.println(getAllPersonne());
+		for(Login login:loginRepository.findAll()) {
+			login.setPassword(passwordEncoder.encode(login.getPassword()));
+			loginRepository.save(login);
+		}
 		
 	}
 
