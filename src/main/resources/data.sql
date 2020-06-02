5f5da4c54789a3054f1fb2d23556182cdcc0bb03
Raphael <raphael.muller@ens.uvsq.fr>
2020-06-02 14:38:14 +0200

    cerrection extval sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,23 +8,23 @@ INSERT INTO public.utilisateur(
 
 INSERT INTO public.localisation(
 	id_localisation, localite, pays, region)
-	VALUES (seq_localisation.nextval, 'france', 'normandie', 'petaouch');
+	VALUES (nextval('seq_localisation'), 'france', 'normandie', 'petaouch');
 
 INSERT INTO public.utilisateur(
 	id_utilisateur, photo_profil, nom, prenom, pseudo, type, version)
-	VALUES (seq_utilisateur.nextval, null, null, null, 'annis', 'ADMIN', 1);
+	VALUES (nextval('seq_utilisateur.nextval'), null, null, null, 'annis', 'ADMIN', 1);
 	
 INSERT INTO public.animal(
 	id_animal, description, emplacement_image, nom_courant, nom_scientifique, ordre)
-	VALUES (seq_animal.nextval, 'cest un chat', null, 'chat', null, null);	
+	VALUES (nextval('seq_animal'), 'cest un chat', null, 'chat', null, null);	
 	
 INSERT INTO public.animal(
 	id_animal, description, emplacement_image, nom_courant, nom_scientifique, ordre)
-	VALUES (seq_animal.nextval, 'cest un chien', null, 'chien', null, null);	
+	VALUES (nextval('seq_animal'), 'cest un chien', null, 'chien', null, null);	
 
 INSERT INTO public.animal(
 	id_animal, description, emplacement_image, nom_courant, nom_scientifique, ordre)
-	VALUES (seq_animal.nextval, 'cest un souris', null, 'souris', null, null);	
+	VALUES (nextval('seq_animal'), 'cest un souris', null, 'souris', null, null);	
 
 	
 INSERT INTO public.observation(
